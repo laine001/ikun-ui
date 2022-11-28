75672699905830512b77adc2f2d18afe27e6d3fa
LAINE001 <laine001@163.com>
2022-11-28 21:49:55 +0800

    fix: vscode settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-essential', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   overrides: [],
   parser: 'vue-eslint-parser',
   parserOptions: {
