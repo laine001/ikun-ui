6555d5a63e12838db7493d66e451f7c621b47e85
LAINE001 <laine001@163.com>
2022-12-20 16:07:46 +0800

    docs: fix

--- scripts/gen.ts
+++ scripts/gen.ts
@@ -6,8 +6,9 @@ const target = path.resolve(__dirname, '../packages/ikui-components/package.json
 const target2 = path.resolve(__dirname, '../packages/ikui-components/README.md')
 
 const content = fs.readFileSync(target, 'utf-8')
-console.log(JSON.parse(content).version)
-
+// const currentVersion = JSON.parse(content).version
+// console.log(currentVersion.split('-beta')[0].split('.'))
+console.log(content)
 fs.copyFileSync(target, path.resolve(__dirname, '../dist/package.json'))
 fs.copyFileSync(target2, path.resolve(__dirname, '../dist/README.md'))
 
