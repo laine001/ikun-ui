21121b990b0674cd2e4e6ea2cd48de55725fe969
LAINE001 <40457081+LAINE001@users.noreply.github.com>
2023-10-08 14:55:48 +0800

    Update index.ts 🌙

--- packages/ikui-components/ripple/index.ts
+++ packages/ikui-components/ripple/index.ts
@@ -1,3 +1,7 @@
-import Ripple as IkRipple from './src/ripple'
+import Ripple from './src/ripple'
+// import { withInstall } from ''
+
+// const IkRipple = withInstall(Ripple)
+const IkRipple = Ripple
 
 export default IkRipple
