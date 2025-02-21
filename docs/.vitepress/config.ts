54fc7fb7e6447ce08be7efa9e1d2885cf70ca6de
laine001 <40457081+laine001@users.noreply.github.com>
2025-02-21 14:14:43 +0800

    ⚡️

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -35,10 +35,11 @@ export default {
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "jwue0owwbp");
+    })(window, document, "clarity", "script", "qd5vq1ckw5");
       `,
     ],
     // ['link', { rel: 'stylesheet', href: 'https://unpkg.com/nes.css/css/nes-core.min.css' }],
+    // 原clarity key jwue0owwbp
   ],
   cleanUrls: true,
   themeConfig: {
